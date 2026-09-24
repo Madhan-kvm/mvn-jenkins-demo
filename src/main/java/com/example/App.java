62a7328b464550b9ae7db9a41dd6189dbc7fa389
root <root@ip-172-31-30-74.ec2.internal>
2026-09-24 08:58:09 +0000

    initial commit ,it is a maven-jenkins-demo project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java + Maven + Jenkins!");
+        System.out.println("My first Jenkins CI/CD project.");
+    }
+}
---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testAddition() {
+        int result = 10 + 20;
+        assertEquals(30, result);
+    }
+}
